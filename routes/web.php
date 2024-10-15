1da7c2a5971575902c939a451084242688aea518
302A-16\cre <a>
2024-10-15 16:07:07 +0900

    タスク編集まで完成させた

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\TaskController;
+
+Route::resource('tasks', TaskController::class);
